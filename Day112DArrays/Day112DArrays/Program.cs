1dfe49656ec72b00f8976d7e20b46aa244578ae3
Roberto Garcia <ing.robertogarcia@hotmail.com>
2018-10-19 16:50:32 -0500

    Day11

---
+++ Day112DArrays/Day112DArrays/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using Day112DArrays.Combinations;
+
+namespace Day112DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = 6;
+            int elementsPerLine = lines;
+
+            int size = 3;
+            int[][] arr = new int[6][]
+            {
+               new int[] {1, 2, 1, 0, 0, 0},
+               new int[] {0, 1, 0, 0, 0, 0},
+               new int[] {1, 1, 1, 0, 0, 0},
+               new int[] {0, 0, 2, 4, 4, 0},
+               new int[] {0, 0, 0, 2, 0, 0},
+               new int[] {0, 0, 1, 2, 4, 0}
+            };
+
+            Console.WriteLine(HigestSumOfArray(arr, size));
+            Console.ReadLine();
+        }
+
+        private static int HigestSumOfArray(int[][] A, int size)
+        {
+            var result = GetHourGlassesArrays(A, size);
+
+            List<int> results = new List<int>();
+            foreach (var arry in result)
+            {
+                results.Add(ToEnumerable<int>(arry).Sum());
+            }
+
+            return results.Max();
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>( Array target)
+        {
+            foreach (var item in target)
+                yield return (T)item;
+        }
+
+
+
+        static int[,][,] GetHourGlassesArrays(int[][] A, int size)
+        {
+            int AResult = 16;
+
+            int[,][,] ArraysOfArrays = new int[4, 4][,];
+
+            for (int i = 0; i < ArraysOfArrays.GetLength(0); i++)
+            {
+                for (int j = 0; j < ArraysOfArrays.GetLength(1); j++)
+                {
+                    ArraysOfArrays[i, j] = new int[size, size];
+                }
+            }
+
+            for (int i = 0; i < ArraysOfArrays.GetLength(0); i++)
+            {
+                for (int j = 0; j < ArraysOfArrays.GetLength(1); j++)
+                {
+                    for (int k = 0; k < size; k++)
+                    {
+                        for (int l = 0; l < size; l++)
+                        {
+                            if (k == 1 && (l == 0 || l == 2))
+                            {
+                                ArraysOfArrays[i, j][k, l] = 0;
+                            }
+                            else
+                            {
+                                ArraysOfArrays[i, j][k, l] = A[k + i][ l + j];
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ArraysOfArrays;
+        }
+
+
+    }
+}
---
+++ Day112DArrays/Day112DArrays/Utils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day112DArrays
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Text;
+
+    namespace Combinations
+    {
+        public  static class Utils
+        {
+          
+
+            public static IEnumerable<IEnumerable<T>> GetCombinations<T>(IEnumerable<T> n, int k)
+            {
+                int i = 0;
+
+                //base
+                foreach (var item in n)
+                {
+                    if (k == 1)
+                    {
+                        yield return new T[] { item };
+
+                    }
+                    //recursive
+                    else
+                    {
+                        foreach (var result in GetCombinations(n.Skip(i + 1), k - 1))
+                        {
+                            yield return new T[] { item }.Concat(result);
+                        }
+                    }
+                }
+
+                ++i;
+
+
+
+            }
+        }
+    }
+}
